7b589facda86e593d1650d6e157581b42127b438
jairo <jairo@example.com>
2024-02-06 22:56:09 -0500

    validTriangle

--- src/tests/Excepciones/TriangleNotPossibleExceptionTest.java
+++ src/tests/Excepciones/TriangleNotPossibleExceptionTest.java
@@ -1,12 +1,20 @@
 package tests.Excepciones;
 
 import org.junit.jupiter.api.*;
+import Exceptions.NegativeSideValuesException;
+import Exceptions.SideWithCeroAsValueException;
+import Exceptions.TriangleNotPossibleException;
+import Triangle.Triangle;  
     
 public class TriangleNotPossibleExceptionTest {
 
+    final String IS_VALID_TRIANGLE_MSG = "No es un triangulo valido";
+    final double VALID_SIDE_VALUE = 1.0;
+    final double equilaterialSide = 3.0;
+
     @Test
-    public void test() {
-        
+    public void testTriangleNotPossibleException() throws TriangleNotPossibleException, SideWithCeroAsValueException, NegativeSideValuesException {
+        TriangleNotPossibleException excepcionEsperada = Assertions.assertThrows(TriangleNotPossibleException.class, () -> new Triangle(VALID_SIDE_VALUE, equilaterialSide, equilaterialSide));
+        Assertions.assertEquals(IS_VALID_TRIANGLE_MSG, excepcionEsperada.getMessage());
     }
 }
-    
